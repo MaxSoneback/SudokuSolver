8f37a912edf0958b35d16fe588e5bfb3ad377c9a
MaxSoneback <maxsoneback@hotmail.com>
2020-01-09 13:31:52 +0100

    added API support

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-start_board = [[0, 0, 9, 7, 0, 0, 0, 0, 6],
-               [0, 0, 7, 0, 9, 0, 0, 4, 2],
-               [0, 2, 4, 6, 0, 3, 0, 0, 0],
-               [0, 0, 0, 4, 0, 0, 9, 0, 0],
-               [6, 0, 0, 0, 0, 0, 0, 0, 1],
-               [0, 0, 5, 0, 0, 8, 0, 0, 0],
-               [0, 0, 0, 9, 0, 4, 2, 7, 0],
-               [4, 5, 0, 0, 3, 0, 8, 0, 0],
-               [7, 0, 0, 0, 0, 6, 3, 0, 0]]
+from board_requester import request_board
 
 
 def solve(board):
@@ -18,7 +10,7 @@ def solve(board):
     else:
         col, row = unfilled_square
 
-        for val in range(1, 10):
+        for val in range(1, len(board)+1):
             if assignment_is_valid(board, val, col, row):
                 board[row][col] = val
 
@@ -107,6 +99,23 @@ def calc_box_limits(col, row):
 
     return range(box_x_start, box_x_end), range(box_y_start, box_y_end)
 
+
+url = 'http://www.cs.utep.edu/cheon/ws/sudoku/new/'
+size = 9
+level = 3  # 1 = enkel, 2 = medel, 3 = svår
+
+start_board = request_board(url, size, level)
+
+if not start_board:
+    start_board = [[0, 0, 9, 7, 0, 0, 0, 0, 6],
+                   [0, 0, 7, 0, 9, 0, 0, 4, 2],
+                   [0, 2, 4, 6, 0, 3, 0, 0, 0],
+                   [0, 0, 0, 4, 0, 0, 9, 0, 0],
+                   [6, 0, 0, 0, 0, 0, 0, 0, 1],
+                   [0, 0, 5, 0, 0, 8, 0, 0, 0],
+                   [0, 0, 0, 9, 0, 4, 2, 7, 0],
+                   [4, 5, 0, 0, 3, 0, 8, 0, 0],
+                   [7, 0, 0, 0, 0, 6, 3, 0, 0]]
 
 print_board(start_board)
 solve(start_board)
